58ac34287cb437609594fb07a0a902152513427a
hy <chessg55@gmail.com>
2019-10-16 18:05:31 +0900

    scss_link

--- caci/js/src/caci_main.js
+++ caci/js/src/caci_main.js
@@ -2,20 +2,20 @@
 
 (function($){
 const mt =  $('.movetop');
-mt.css({zIndex:'1000'});
+// mt.css({zIndex:'1000'});
 const topBtn = $('.movetop').children('a');
 
-topBtn.css({"backgroundColor":'#fa0', 'position':'relative', 'zIndex':'1000'});
-topBtn.children().hide();
+// topBtn.css({"backgroundColor":'#fa0', 'position':'relative', 'zIndex':'1000'});
+// topBtn.children().hide();
 
 
-topBtn.on('mouseenter',function(event){
-   // event.preventDefault();
+topBtn.on('click',function(event){
+   event.preventDefault();
    console.log(event);
    // $(window).scrollTop(0);
    // $(window).animate({'scrollTop':0});
-   // $('html').animate({'scrollTop':0},100);
-   $(this).css({"backgroundColor":'#0a0'})
+   $('html').animate({'scrollTop':0},1000);
+   // $(this).css({"backgroundColor":'#0a0'})
    });
 
 })(jQuery);
