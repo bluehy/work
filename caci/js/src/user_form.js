b5e17b9c105a688abfe7e694988adddb431b9e3d
hy <chessg55@gmail.com>
2019-11-11 17:55:17 +0900

    template_seperate_scss_upload

--- caci/js/src/user_form.js
+++ caci/js/src/user_form.js
@@ -1,18 +1,10 @@
-// main.js
 (function($){
    const wrap = $('#wrap');
-   wrap.prepend('<header id="headBox"></header>');
+   wrap.prepend('<header id ="headBox"></header>');
+
    const headBox = wrap.children('#headBox');
 
-   // headBox
    headBox.load('./temp/headBox.html');
-
-   
-   // main
-
-
-
-   // footer
 
 
 })(jQuery);
