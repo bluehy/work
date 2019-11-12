0c27b911e60beb6642b34ba85a2dea3e83b5e354
hy <chessg55@gmail.com>
2019-11-12 16:38:30 +0900

    footer_modify

--- caci/js/src/user_form.js
+++ caci/js/src/user_form.js
@@ -1,10 +1,17 @@
 (function($){
+   const body = $('body');
    const wrap = $('#wrap');
    wrap.prepend('<header id ="headBox"></header>');
+   wrap.append('<section id ="viewBox"></section>');
+   wrap.append('<main id="conBox"</main>');
+   wrap.append('<footer id ="footBox"></footer>');
 
    const headBox = wrap.children('#headBox');
+   const viewBox = wrap.children('#viewBox');
+   const conBox = wrap.children('#conBox');
+   const footBox = wrap.children('#footBox');
 
    headBox.load('./temp/headBox.html');
-
+   footBox.load('./temp/footBox.html');
 
 })(jQuery);
