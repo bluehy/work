57484e0f38bc68c9819b8f9f8c2d2f2f9cb99197
hy <chessg55@gmail.com>
2019-11-13 11:40:48 +0900

    headBox_gnb_navMenu_

--- caci/js/src/main.js
+++ caci/js/src/main.js
@@ -3,6 +3,7 @@
    const wrap = $('#wrap');
    wrap.prepend('<header id="headBox"></header>');
    const headBox = wrap.children('#headBox');
+   wrap.append('<footer id="footBox"></footBox>');
    const footBox = wrap.children('#footBox');
 
    // headBox
