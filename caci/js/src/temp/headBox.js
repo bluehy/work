fbb490981111904e0c405bf434829348d3c734fd
hy <chessg55@gmail.com>
2019-11-21 10:39:30 +0900

    sideGnb_rwd_js_move to headBox

--- caci/js/src/temp/headBox.js
+++ caci/js/src/temp/headBox.js
@@ -41,7 +41,7 @@
 
 
 // -----------------------------------------------------
-
+   const body = $('body');
    const headBox = $('#headBox');
    const headWrap = headBox.children('.head_wrap');
    const gnb = headWrap.children('#gnb');
@@ -186,6 +186,81 @@ pageLi.eq(0).addClass('action');
 // ------------------------------------------------------
 
 
+   // jQuery device 사이즈 측정
+   // 각 디바이스별 기준 사이즈 지정
+   let mob = 480, tab = 768, laptop = 1280, pc = 1920;
+
+   let nowSize;
+   // 사이즈 범주별 디바이스 명칭
+   const device = ['mobile', 'tablet', 'laptop', 'pc', 'pcfull'];
+
+   let beforeW = $(window).outerWidth(true);
+
+   //디바이스별 data 처리
+   const DeviceData = function (wid) {
+      switch (wid) {
+         case device[0]:
+
+         case device[1]:
+            body.append('<script src="../js/src/temp/headBox_768.js"></script>');
+            break;
+
+         case device[2]:
+         case device[3]:
+         case device[4]:
+            break;
+      }//switch
+
+   };//DeviceData();
+
+
+   // ------------------------------------------------
+   //디바이스 크기 체크 및 명칭 판단.
+   const DeviceSet = function (winW) {
+      if (winW <= mob) {
+         nowSize = device[0];
+      } else if (winW > mob && winW <= tab) {
+         nowSize = device[1];
+      } else if (winW > tab && winW <= laptop) {
+         nowSize = device[2];
+      } else if (winW > laptop && winW <= pc) {
+         nowSize = device[3];
+      } else {
+         nowSize = device[4];
+      }
+      return nowSize;
+   };//DeviceSet()
+   let beforeDevice = DeviceSet(beforeW);
+
+
+
+   // ------------------------------------------------
+   //브라우저가 파이어폭스인지 검증
+   let browser = navigator.userAgent.toLowerCase();
+   let nowb;
+
+   if (browser.indexOf('firefox') !== -1) {
+      nowb = 'firefox';
+   } else {
+      nowb = 'other';
+   };
+   // ------------------------------------------------
+   // 브라우저 사이즈 변경시, 기준 디바이스 변경.
+   $(window).on('resize', function () {
+      let afterW = $(window).outerWidth(true);
+      let afterDevice = DeviceSet(afterW);
+
+      if (beforeDevice !== afterDevice) {
+         if (nowb == 'firefox') {
+            window.location = window.location;
+         } else {
+            location.reload();
+         }//if(nowb == 'firefox')
+      }//if(beforeDevice !== afterDevice)
+   })//$(window).on('resize')
+   
+   
+   DeviceData(beforeDevice);
 
 
 
