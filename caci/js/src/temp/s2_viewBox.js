8b8650c7ca1022b235a7b03cff13065a9e7bd745
hy <chessg55@gmail.com>
2019-11-25 17:05:46 +0900

    subPage_basic_setting_subPageIndi_

--- caci/js/src/temp/s2_viewBox.js
+++ caci/js/src/temp/s2_viewBox.js
@@ -1,3 +1,19 @@
 (function($){
+   
+   
+   const viewBox = $('#viewBox');
+   const viewWrap = viewBox.children('.view_wrap');
+
+   const pageBtn = viewWrap.find('.page_btn');
+   
+
+// ----------------------------------------
+   viewBox.children('h2').text('충무아트센터 회원가입 페이지');
+
+   let imgUrl = "../img/sub2/";
+   let viewImgUrl = "viewBox/viewImg";
+   
+   viewWrap.css({'backgroundImage':'url("'+ imgUrl + viewImgUrl +'.jpg")'});
+
 
 })(jQuery);
