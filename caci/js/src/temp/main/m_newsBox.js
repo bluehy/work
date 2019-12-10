125274482a413f9d93e53810a88d639f7c0c4d4a
hy <chessg55@gmail.com>
2019-12-10 10:54:03 +0900

    rwd_main_section_edit

--- caci/js/src/temp/main/m_newsBox.js
+++ caci/js/src/temp/main/m_newsBox.js
@@ -8,12 +8,22 @@
 // ------------------------------------------------
 // 스크롤 내리기 시작하면, 뉴스박스 사라지기
    $(window).on('scroll',function(){
-      winScroll = $(window).scrollTop();
-      if(winScroll > 100){
-         newsBox.slideUp();
+      let rel;
+      winWidth = $(window).outerWidth();
+      console.log(winWidth);
+      if(winWidth <= 960){
+         rel=false;
       }else{
-         // newsBox.slideDown();
+         rel=true;
       }
+      if(rel){
+         winScroll = $(window).scrollTop();
+         if(winScroll > 100){
+            newsBox.slideUp();
+         }else{
+            newsBox.slideDown();
+         }
+      }      
    });
 
 
